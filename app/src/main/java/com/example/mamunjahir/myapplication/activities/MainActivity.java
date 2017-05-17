f6582ff3b8264e6afa740e29718a6bff0b6ac4bd
Mamunjahir <mamunjahir.com@gmail.com>
2017-05-16 22:46:51 -0400

    Some descriptions Added

--- app/src/main/java/com/example/mamunjahir/myapplication/activities/MainActivity.java
+++ app/src/main/java/com/example/mamunjahir/myapplication/activities/MainActivity.java
@@ -9,16 +9,28 @@ import com.example.mamunjahir.myapplication.R;
 import com.example.mamunjahir.myapplication.Views.ViewPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
-    ViewPager mainviewpager;
+    ViewPager viewpager;
     TabLayout tabLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mainviewpager= (ViewPager) findViewById(R.id.activity_main_viewPager);
+        viewpager= (ViewPager) findViewById(R.id.activity_main_viewPager);
        tabLayout= (TabLayout) findViewById(R.id.activity_tab_layout);
         ViewPagerAdapter adapter =new ViewPagerAdapter(getSupportFragmentManager());
-       mainviewpager.setAdapter(adapter);
-tabLayout.setupWithViewPager(mainviewpager);
+       viewpager.setAdapter(adapter);
+tabLayout.setupWithViewPager(viewpager);
     }
 }
+
+
+/* We need a tab layout to setupWithViewPager
+For viewPager wee need ViewPagerAdapter.There are two ViewPager Adapter
+1.FragmentPagerAdapter or 2.FragmentStatePagerAdapter.
+=> When you implement a PagerAdapter, you must override the following methods at minimum:
+instantiateItem(ViewGroup, int)
+destroyItem(ViewGroup, int, Object)
+getCount()
+isViewFromObject(View, Object)
+
+*/
